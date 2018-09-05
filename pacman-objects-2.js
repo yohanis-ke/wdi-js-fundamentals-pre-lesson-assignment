5ac2f607ed1f851b91081d0cd969a8692d0385a4
yohanis-ke <yohanes.ke@gmail.com>
2018-09-04 22:45:11 -0400

    Tuesday, Sep 4th 03 - Intro to JavaScript

---
+++ pacman-objects-2.js
@@ -0,0 +1,9 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+console.log(ghosts.inky); // => 'Cyan'
+console.log(ghosts.blinky); // => 'Red'
+
+// same as above
+console.log(ghosts['inky']); // => 'Cyan'
+console.log(ghosts['blinky']); // => 'Red'
